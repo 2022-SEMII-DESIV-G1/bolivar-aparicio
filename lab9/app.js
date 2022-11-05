08d1d904adc700483eb9ceae0ca9758bf3b8d57e
Bolivar Aparicio <alexiiz360@gmail.com>
2022-11-05 17:22:20 -0500

    updates

---
+++ lab9/app.js
@@ -0,0 +1,11 @@
+function buscar(){
+    var i, rep;
+    for (i = 1; i <= 15; i++) {
+        for (rep = 0; rep < i; rep++) {
+            
+            a="*";
+            document.write(a);
+        }
+        document.write("<br>");
+    }
+}
