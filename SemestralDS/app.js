fc4f4e6c25d28a1d8d49750383e6110bb2eeaec0
Bolivar Aparicio <alexiiz360@gmail.com>
2022-12-06 17:24:44 -0500

    update

--- SemestralDS/app.js
+++ SemestralDS/app.js
@@ -1,9 +1,9 @@
-const Semestral= document.querySelector('.Semestral');
+        const Semestral= document.querySelector('.Semestral');
         const form = document.querySelector('#piramide-form');
         const name = document.querySelector('#name');
         const height = document.querySelector("#height");
         const piramide = document.querySelector('#piramide');
-        const array = piramide.value.split(",");
+        // const array = piramide.value.split(",");
         const render = (piramidesResponse)=>
         {   let newList = '<ul>';
             for( const piramide of piramidesResponse)
@@ -13,6 +13,7 @@ const Semestral= document.querySelector('.Semestral');
             newList+='</ul>'
             Semestral.innerHTML =newList;
         }
+        
 
         form.addEventListener('submit',async (e)=>
         {
@@ -25,50 +26,4 @@ const Semestral= document.querySelector('.Semestral');
             const {data} = await axios.get('http://localhost:4567/Semestral');
             render(data.piramides);
         })();
-
-// const url = "http://localhost:4567/";
-// const POSTUrl = "PostPyramid";
-// const GET = "GetAllPyramid";
-// const GETById = "GetPyramidById/";
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const array = pyramidForm.value.split(",");
-//   let cont = 0;
-//   for (i = 0; i < height.value; i++) {
-//     cont = cont + (i + 1);
-//   }
-
-//   console.log(cont);
-
-//   console.log(array.length);
-//   if (array.length != cont) {
-//     validationText.style.display = "block";
-//   } else {
-//     const params = {
-//       pyramid: "",
-//       solution: "1,2,3,4",
-//     };
-//     params.pyramid = pyramidForm.value;
-//     POST(params);
-//   }
-// });
-
-// const GETPyramids = () => {
-//   const res = axios.get(url + GET);
-//   console.log(res);
-// };
-
-// const GETPyramidsById = (id) => {
-//   const res = axios.get(url + GETById + id);
-//   console.log(res);
-// };
-
-// const POST = (params) => {
-//   const res = axios.post(url + POSTUrl, params).then((response) => {
-//     if (response.status === 200) {
-//       console.log("SUCCES!");
-//     } else "ERROR!";
-//   });
-//   console.log(res);
-// };
+      
